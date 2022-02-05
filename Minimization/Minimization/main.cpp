b71d8ac550bac8b949434105d5b5e373bc840d3f
Insaf4ik <insaf.halirahmanoff@yandex.ru>
2022-02-05 12:52:29 +0300

    Minimization

---
+++ Minimization/Minimization/main.cpp
@@ -0,0 +1,282 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include<algorithm>
+#include <map>
+
+using namespace std;
+
+const int SETW_VALUE = 20;
+
+struct Cell
+{
+	int multiplicity;
+	int to;
+	int out;
+};
+
+typedef map<int, vector<int>> EMatrix;
+typedef vector<vector<Cell>> Machine;
+
+EMatrix matrix;
+Machine ResultMachine;
+vector<int> ResultIndexes;
+
+void GetEquivalenceMatrix(Machine& machine, vector<int>& indexes, EMatrix& equivalenceMatrix)
+{
+	int k = 0;
+	equivalenceMatrix[k].push_back(indexes[0]);
+
+	for (int i = 1; i < indexes.size(); i++)
+	{
+		bool isFound = false;
+		EMatrix::iterator it = equivalenceMatrix.begin();
+
+		while (it != equivalenceMatrix.end() && !isFound)
+		{
+			isFound = true;
+			for (int j = 0; j < machine.size(); j++)
+			{
+				if (machine[j][it->second[0]].multiplicity != machine[j][indexes[i]].multiplicity)
+				{
+					isFound = false;
+					it++;
+					break;
+				}
+			}
+		}
+
+		isFound
+			? equivalenceMatrix[it->first].push_back(indexes[i])
+			: equivalenceMatrix[++k].push_back(indexes[i]);
+	}
+}
+
+void OverrideAutomaton(Machine& machine, EMatrix& equivalenceMatrix)
+{
+	for (int i = 0; i < machine.size(); i++)
+	{
+		for (int j = 0; j < machine[0].size(); j++)
+		{
+			auto it = equivalenceMatrix.begin();
+			while (find((it->second).begin(), (it->second).end(), machine[i][j].to) == (it->second).end())
+			{
+				it++;
+			}
+			machine[i][j].multiplicity = it->first;
+		}
+	}
+}
+
+void GetResult(Machine& machine, EMatrix& ResultMatrix)
+{
+	Machine result(machine.size(), vector<Cell>(ResultMatrix.size()));
+	EMatrix::iterator it = ResultMatrix.begin();
+
+	for (int i = 0; i < result[0].size(); i++)
+	{
+		for (int j = 0; j < result.size(); j++)
+		{
+			result[j][i] = machine[j][it->second[0]];
+
+			auto n = ResultMatrix.begin();
+			while (find((n->second).begin(), (n->second).end(), machine[j][it->second[0]].to) == (n->second).end())
+			{
+				n++;
+			}
+			result[j][i].to = n->second[0];
+		}
+		ResultIndexes.push_back(it->second[0]);
+		it++;
+	}
+
+	ResultMachine = result;
+}
+
+void Minimize(Machine& machine, int& prevMachineSize)
+{
+	bool isNeedMinimize = true;
+	EMatrix equivalenceMatrix;
+	for (auto it = matrix.begin(); it != matrix.end(); it++)
+	{
+		EMatrix matrixE;
+		GetEquivalenceMatrix(machine, it->second, matrixE);
+
+		int k = equivalenceMatrix.size();
+		for (auto arr : matrixE)
+		{
+			equivalenceMatrix[k] = arr.second;
+			k++;
+		}
+	}
+	matrix = equivalenceMatrix;
+
+	OverrideAutomaton(machine, equivalenceMatrix);
+	if (equivalenceMatrix.size() == prevMachineSize)
+	{
+		isNeedMinimize = false;
+		GetResult(machine, equivalenceMatrix);
+	}
+
+	prevMachineSize = equivalenceMatrix.size();
+	if (isNeedMinimize)
+	{
+		Minimize(machine, prevMachineSize);
+	}
+}
+
+void ReadMilli(Machine& a, ifstream& input)
+{
+	string lineS;
+	string lineY;
+	getline(input, lineS);
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		getline(input, lineS);
+		replace_if(lineS.begin(), lineS.end(), isalpha, ' ');
+
+		getline(input, lineY);
+		replace_if(lineY.begin(), lineY.end(), isalpha, ' ');
+
+		stringstream strS(lineS);
+		stringstream strY(lineY);
+
+		int ar = a[0].size();
+		for (int j = 0; j < a[0].size(); j++)
+		{
+			int numS;
+			strS >> numS;
+			a[i][j].to = numS - 1;
+
+			int numY;
+			strY >> numY;
+			a[i][j].multiplicity = numY - 1;
+			a[i][j].out = numY - 1;
+		}
+	}
+}
+
+void ReadMurra(Machine& a, ifstream& input)
+{
+	string lineS;
+	string lineY;
+	getline(input, lineS);
+
+	getline(input, lineY);
+	replace_if(lineY.begin(), lineY.end(), isalpha, ' ');
+	stringstream strY(lineY);
+
+	for (int i = 0; i < a[0].size(); i++)
+	{
+		int numY;
+		strY >> numY;
+
+		for (int j = 0; j < a.size(); j++)
+		{
+			a[j][i].multiplicity = numY - 1;
+			a[j][i].out = numY - 1;
+		}
+	}
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		getline(input, lineS);
+		replace_if(lineS.begin(), lineS.end(), isalpha, ' ');
+		stringstream strS(lineS);
+
+		for (int j = 0; j < a[0].size(); j++)
+		{
+			int numS;
+			strS >> numS;
+			a[i][j].to = numS - 1;
+		}
+	}
+}
+
+Machine ReadMachine(ifstream& input, string type)
+{
+	int x, y, r;
+	input >> y >> x >> r;
+	Machine machine(x, vector<Cell>(y));
+
+	type == "Ml"
+		? ReadMilli(machine, input)
+		: ReadMurra(machine, input);
+	return machine;
+}
+
+void WriteMilli(ofstream& output)
+{
+	for (int i = 0; i < ResultIndexes.size(); i++)
+	{
+		output << setw(SETW_VALUE) << "S" + to_string(ResultIndexes[i] + 1);
+	}
+	output << endl;
+
+	for (int i = 0; i < ResultMachine.size(); i++)
+	{
+		output << "X" << i + 1;
+		for (int j = 0; j < ResultMachine[0].size(); j++)
+		{
+			output << setw(SETW_VALUE) << right << "[S" + to_string(ResultMachine[i][j].to + 1) + ", Y" + to_string(ResultMachine[i][j].out + 1) + "]";
+		}
+		output << endl;
+	}
+}
+
+void WriteMurra(ofstream& output)
+{
+	for (int i = 0; i < ResultIndexes.size(); i++)
+	{
+		output << setw(SETW_VALUE) << "Y" + to_string(ResultMachine[0][i].out + 1);
+	}
+	output << endl;
+
+	for (int i = 0; i < ResultIndexes.size(); i++)
+	{
+		output << setw(SETW_VALUE) << "S" + to_string(ResultIndexes[i] + 1);
+	}
+	output << endl;
+
+	for (int i = 0; i < ResultMachine.size(); i++)
+	{
+		output << "X" << i + 1;
+		for (int j = 0; j < ResultMachine[0].size(); j++)
+		{
+			string out = "[S" + to_string(ResultMachine[i][j].to + 1) + "]";
+			output << setw(SETW_VALUE) << right << out;
+		}
+		output << endl;
+	}
+}
+
+void WriteMachine(string type)
+{
+	ofstream output("output.txt");
+	type == "Ml"
+		? WriteMilli(output)
+		: WriteMurra(output);
+}
+
+int main()
+{
+	string type;
+	ifstream input("input.txt");
+	getline(input, type);
+
+	auto machine = ReadMachine(input, type);
+
+	bool isMinimized = false;
+	int machineSize = machine[0].size();
+	for (int i = 0; i < machineSize; i++)
+	{
+		matrix[0].push_back(i);
+	}
+
+	Minimize(machine, machineSize);
+	WriteMachine(type);
+}
